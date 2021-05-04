54f380c9917e2b86a700f5f484f0fe75c5131bb9
Barzi Jaf <barzi4greatness@gmail.com>
2021-05-04 18:30:15 +0200

    Works like an m5 engine

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const numberInput = document.querySelector("#number-input");
-
 const convertBtn = document.querySelector("button");
+const resultText = document.querySelector("#result");
+
+let inDecimal, arrayInput;
 
 convertBtn.addEventListener("click", function (e) {
   e.preventDefault();
+
+  inDecimal = 0; // to reset
+  arrayInput = numberInput.value.split("");
+  arrayInput.reverse();
+
+  for (i = 0; i < arrayInput.length; i++) {
+    if (arrayInput[i] != "0" && arrayInput[i] != "1") {
+      resultText.innerHTML = `<span style='color: red'>Input is not binary</span>`;
+      break;
+    } else {
+      inDecimal += Math.pow(2 * arrayInput[i], i);
+      resultText.innerHTML = `${numberInput.value} in binary is <strong>${inDecimal}</strong>`;
+    }
+  }
 });
