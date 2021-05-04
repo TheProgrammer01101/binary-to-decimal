eac08200fe7f2cb0f97116f11cb8599a84f65f9a
Barzi Jaf <barzi4greatness@gmail.com>
2021-05-04 16:02:14 +0200

    setup

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberInput = document.querySelector("#number-input");
+
+const convertBtn = document.querySelector("button");
+
+convertBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+});
